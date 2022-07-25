43dfd34aa2f18cc2d3efe8859d9e5114b0f443fe
Paolo Pedrigal <paolopedrigal@gmail.com>
2022-07-24 18:03:59 -0700

    Started working on Projects component.

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,14 @@
 export default function Projects() {
     return (
-        <h2>Projects</h2>
+        <div>
+            <h2>Projects</h2>
+            <ProjectPredictingReviews />
+        </div>
     );
   }
+
+function ProjectPredictingReviews() {
+    return (
+        <h3>Predicting Universal Studios Reviews</h3>
+    )
+}
