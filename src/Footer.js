3224caaed9f31e796378997fe2446031fd865041
Paolo Pedrigal <paolopedrigal@gmail.com>
2022-08-24 06:18:17 -0700

    Finished styling Footer section. Need to fix Email link though.

--- src/Footer.js
+++ src/Footer.js
@@ -1,21 +1,23 @@
-function Footer() {
-    return(
-        <div>
-            <h3>Contact Me via...</h3>
-            <ul>
-                <li>
-                    <a href="https://linkedin.com/in/paolopedrigal">LinkedIn</a>
-                </li>
-                <li>
-                    <a href="https://github.com/paolopedrigal">GitHub</a>
-                </li>
-                <li>
-                    <a href="mailto:paolopedrigal@gmail.com">Email</a>
-                </li>
-            </ul>
-            <h3>Created by Paolo Pedrigal.</h3>
-        </div>
-    )
-}
+import './Footer.css';
 
-export default Footer;
+export default function Footer() {
+    return(
+        <div id='footer-section'>
+            <div style={{backgroundColor:"transparent"}}>
+                <ul id='contact-list'>
+                    <li id='contact-me-via'>Contact Me via...</li>
+                    <li className='contact-link-container'>
+                        <a href="https://linkedin.com/in/paolopedrigal" target="_blank" className='contact-link'>LinkedIn</a>
+                    </li>
+                    <li className='contact-link-container'>
+                        <a href="https://github.com/paolopedrigal" target="_blank" className='contact-link'>GitHub</a>
+                    </li>
+                    <li className='contact-link-container'>
+                        <a href="mailto:paolopedrigal@gmail.com" target="_blank" className='contact-link'>Email</a>
+                    </li>
+                </ul>
+            </div>
+            <h3 id='created-by-paolo-pedrigal'>Created by Paolo Pedrigal.</h3>
+        </div>
+    );
+}
