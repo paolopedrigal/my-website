04ae3488bdc8357ec13c5124c46f1bca9d03da36
Paolo Pedrigal <paolopedrigal@gmail.com>
2022-08-28 23:27:53 -0700

    Finally finished styling the entire website (without any animations)

--- src/Experiences.js
+++ src/Experiences.js
@@ -1,6 +1,6 @@
 import MCHSImage from './Images/MCHSWideShot.webp';
 import StanfordImage from './Images/StanfordDataScience.png';
-import UCSBImage from './Images/UCSBDataScience.png';
+import UCSBImage from './Images/UCSBDataScience2.png';
 import MLioraImage from './Images/MLioraLLCLogo.jpg';
 import SkyworksImage from './Images/SkyworksLogo.jpg';
 import './Experiences.css';
@@ -10,77 +10,51 @@ function Timeline() {
         <div id='timeline-section'>
             <h3 id='timeline-title'>Timeline</h3>
             <div id='timeline-content'>
-                <div>
-                    <span className='placeholder'></span>
-                    <span className='timeline-end-line'></span>
-                </div>
+                <div className='timeline-line-break' style={{marginLeft: "48%"}}></div>
                 <div id='before-june-2021'>
                     <div id='lhs-before-june-2021'>
-                        <div id='above-line-lhs-before-june-2021'>
-                            <h3 className='timeline-date'>May 2018</h3>
-                        </div>
-                        <div id='below-line-lhs-before-june-2021'>
-                            <p className='timeline-caption'>Graduated from Moreau Catholic High School.</p>
-                            <img src={StanfordImage} width='300px' alt='Stanford Department of Biomedical Data Science logo.' className='timeline-content'
-                            id='Stanford'/>
-                            <p className='click-for-details'>Click for details.</p>
-                        </div>
+                        <h3 className='timeline-date' id='may-2018'>May 2018</h3>
+                        <div className='timeline-line-break' style={{marginLeft: "91%"}}></div>
+                        <p className='timeline-caption' id='mchs-caption'>Graduated from Moreau Catholic High School</p>
+                        <img src={StanfordImage} width='300px' alt='Stanford Department of Biomedical Data Science logo.' className='timeline-content'
+                        id='stanford-pic'/>
+                        <p className='click-for-details' style={{marginRight: "100px"}}>Click for details.</p>
                     </div>
                     <div id='rhs-before-june-2021'>
-                        <div id='above-line-rhs-before-june-2021'>
-                            <img src={MCHSImage} width='300px' alt='A wide shot of Moreau Catholic High School.' className='timeline-content' id='MCHS'/>
-                            <h3 className='timeline-date'>April 2021</h3>
-                        </div>
-                        <div id='below-line-rhs-before-june-2021'>
-                            <p className='timeline-caption'>Data Science Mentorship @ Stanford Medicine, Department of Biomedical Data Science</p>
-                        </div>
+                        <img src={MCHSImage} width='300px' alt='A wide shot of Moreau Catholic High School.' className='timeline-content' id='mchs-pic'/>
+                        <h3 className='timeline-date' id='april-2021'>April 2021</h3>
+                        <div className='timeline-line-break'></div>
+                        <p className='timeline-caption' id='stanford-caption'>Data Science Mentorship @ Stanford Medicine, Department of Biomedical Data Science</p>
                     </div>
                 </div>
-                
-                <p id='june-2021'>June 2021</p>
+
+                <p className='timeline-date' id='june-2021'>June 2021</p>
 
                 <div id='after-june-2021'>
                     <div id='lhs-after-june-2021'>
-                        <div id='above-line-lhs-after-june-2021'></div>
-                        <div id='below-line-lhs-after-june-2021'></div>
+                        <p className='timeline-caption' id='ucsb-caption'>Data Science Internship @ University of California, Santa Barbara (Remote)</p>
+                        <img src={UCSBImage} width='300px' alt='UCSB Data Science logo.' className='timeline-content' id='ucsb-pic'/>
+                        <p className='click-for-details' style={{marginRight: "50px"}}>Click for details.</p>
+                        <h3 className='timeline-date' id='june-2022'>June 2022</h3>
+                        <div className='timeline-line-break' style={{marginLeft: "91%"}}></div>
+                        <p className='timeline-caption' id='skyworks-caption'>Product/Test Engineering Software Internship @ Skyworks Solutions Inc., Irvine, CA</p>
                     </div>
-                    <div id='rhs-after-june-2021'></div>
-                </div>
-                {/* 
-                <UCSB className='timeline-content'/>
-                <MLiora className='timeline-content'/>
-                <Skyworks className='timeline-content'/>
-                <PresentDay/> Added the className timeline-content to the functional component itself */}
+                    <div id='rhs-after-june-2021'>
+                        <p className='timeline-caption' id='mliora-caption'>UI Designer/Front-End Developer Internship @ MLiora LLC, San Jose, CA (Remote)</p>
+                        <img src={MLioraImage} width='300px' alt='MLiora logo.' className='timeline-content' id='mliora-pic'/>
+                        <p className='click-for-details'>Click for details.</p>
+                        <img src={SkyworksImage} width='300px' alt='Skyworks logo.' className='timeline-content' id='skyworks-pic'/>
+                        <p className='click-for-details'>Click for details.</p>
+                    </div>
+                </div>        
+                <p className='timeline-date' id='present-day'>Present Day</p>
+                <p className='timeline-caption' style={{textAlign:"center"}}>Currently, I am building a mobile app that tracks fitness goals!</p>
             </div>
-
         </div>
             
     );
 }
 
-function UCSB() {
-    return (
-        <img src={UCSBImage} width='300px' alt='UCSB Data Science logo.' className='timeline-content' id='UCSB'/>
-    );
-}
-
-function MLiora() {
-    return (
-        <img src={MLioraImage} width='300px' alt='MLiora logo.' className='timeline-content' id='MLiora'/>
-    );
-}
-
-function Skyworks() {
-    return (
-        <img src={SkyworksImage} width='300px' alt='Skyworks logo.' className='timeline-content' id='Skyworks'/>
-    );
-}
-
-function PresentDay() {
-    return (
-            <p id='present-day'>Present Day</p>
-    )
-}
 
 // function ExperienceSkyworksCard() {
 //     return (
