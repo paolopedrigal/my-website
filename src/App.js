037a03face46fea006ff0efdb9791b19644a55ee
Paolo Pedrigal <paolopedrigal@gmail.com>
2022-11-14 01:07:50 -0800

    Added a basic node-collision

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import AboutMe from './AboutMe.js';
 import Skills from './Skills.js';
 import Timeline from './Experiences.js';
 import Projects from './Projects.js';
-import Footer from './Footer.js'
+import Footer from './Footer.js';
+
+import Canvas from "./Canvas.js";
 import './App.css';
 
 function App() {
@@ -17,6 +19,7 @@ function App() {
       <Timeline />
       <Projects />
       <Footer />
+      <Canvas />
     </div>
   );
 }
--- src/Canvas.js
+++ src/Canvas.js
@@ -1,1 +1,41 @@
-// test
+import { useRef, useEffect } from "react";
+import Nodes from "./canvas-nodes.js";
+
+function Canvas() {
+
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        console.log("Test", canvas);
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight; 
+        const context = canvas.getContext("2d");   
+        const nodes = new Nodes(10, context, 100, "white");
+        nodes.spawnNodes();
+        
+        window.addEventListener('resize', () => { // updates the canvas's screen when the browser is resized
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+          
+            // init() //   <--- Potentially uncomment this
+        })
+
+        function animate() {
+            requestAnimationFrame(animate);
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            nodes.updateNodes();
+        }
+
+        animate();
+    })
+
+
+    return(
+        <canvas 
+            ref={canvasRef}>
+        </canvas>
+    );
+}
+
+export default Canvas;
