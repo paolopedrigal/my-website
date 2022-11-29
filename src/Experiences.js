21a51ccf7992c5b85937af61b06158da30ee6dad
Paolo Pedrigal <paolopedrigal@gmail.com>
2022-11-29 09:33:35 -0800

    Finished (a first version draft) of the CSS styling for the experience/timeline section

--- src/Experiences.js
+++ src/Experiences.js
@@ -31,22 +31,50 @@ function Timeline() {
                 <h3 className='timeline-date'>May 2018</h3>
                 <p className='timeline-caption'>Graduated from Moreau Catholic High School</p>
 
+                <div className='timeline-line'>
+                    <div className='line'></div>
+                    <div></div>
+                </div>
+
                 <img src={StanfordImage} alt='Stanford Department of Biomedical Data Science logo.' className='timeline-pic'/>
                 <h3 className='timeline-date'>April 2021</h3>
-                <p className='timeline-caption'>Data Science Mentorship @ Stanford Medicine, Department of Biomedical Data Science</p>
+                <p className='timeline-caption'>Data Science Mentorship</p>
+                <p className='timeline-caption location' id='stanford-caption'>@ Stanford Medicine, Department of Biomedical Data Science</p>
             
+                <div className='timeline-line'>
+                    <div className='line'></div>
+                    <div></div>
+                </div>
 
                 <img src={UCSBImage} alt='UCSB Data Science logo.' className='timeline-pic'/>
-                <p className='timeline-date' id='june-2021'>June 2021</p>
-                <p className='timeline-caption'>Data Science Internship @ University of California, Santa Barbara (Remote)</p>
+                <p className='timeline-date'>June 2021</p>
+                <p className='timeline-caption'>Data Science Internship</p> 
+                <p className='timeline-caption location' id='ucsb-caption'>@ University of California, Santa Barbara (Remote)</p>
+
+                <div className='timeline-line'>
+                    <div className='line'></div>
+                    <div></div>
+                </div>
 
                 <img src={MLioraImage} width='300px' alt='MLiora logo.' className='timeline-pic'/>
-                <p className='timeline-date' id='june-2021'>June 2021</p>
-                <p className='timeline-caption' id='mliora-caption'>UI Designer/Front-End Developer Internship @ MLiora LLC, San Jose, CA (Remote)</p>
+                <p className='timeline-date'>June 2021</p>
+                <p className='timeline-caption'>UI Designer/Front-End Developer Internship</p>
+                <p className='timeline-caption location' id='mliora-caption'>@ MLiora LLC, San Jose, CA (Remote)</p>
+
+                <div className='timeline-line'>
+                    <div className='line'></div>
+                    <div></div>
+                </div>
 
                 <img src={SkyworksImage} width='300px' alt='Skyworks logo.' className='timeline-pic'/>
                 <h3 className='timeline-date'>June 2022</h3>
-                <p className='timeline-caption'>Product/Test Engineering Software Internship @ Skyworks Solutions Inc., Irvine, CA</p>
+                <p className='timeline-caption'>Product/Test Engineering Software Internship</p>
+                <p className='timeline-caption location' id='skyworks-caption'>@ Skyworks Solutions Inc., Irvine, CA</p>
+
+                <div className='timeline-line'>
+                    <div className='line'></div>
+                    <div></div>
+                </div>
 
                 <p className='timeline-date' id='present-day'>Present Day</p>
                 <p className='timeline-caption' style={{textAlign:"center"}}>Currently, I am building a mobile app that tracks fitness goals!</p>
